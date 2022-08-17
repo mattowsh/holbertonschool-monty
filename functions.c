4a9eeea491f88bc9124c884e342de208c2b11c8e
TotiFlame <santifer4321@gmail.com>
2022-08-17 14:45:18 -0700

    Creation of pall, pint and pop

---
+++ functions.c
@@ -0,0 +1,47 @@
+#include "monty.h"
+/**
+* _pall - print all the values on the stack
+* from the top of the stack.
+* @stack: double linked list
+* @line_number: line number of the .m file
+*/
+void _pall(stack_t **stack, unsigned int line_number)
+{
+	int count = 0;
+	stack_t *h = *stack;
+	
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+}
+
+/**
+* _pint - prints the value at the top
+* of the stack.
+* @stack: double linked list
+* @line_number: line number of the .m file
+*/
+void _pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *h = *stack;
+
+	// verificar si es numero antes de printear
+	printf("%d\n", h->n);
+}
+
+/**
+* _pop - removes the top element of the stack
+* @stack: double linked list
+* @line_number: line number of the .m file
+*/
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *h = *stack;
+
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(h);
+}
