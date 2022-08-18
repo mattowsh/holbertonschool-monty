86f2c42f0a9993da7157e3c94f9406e45ef3e3c7
mattowsh <5089@holbertonschool.com>
2022-08-18 15:46:02 -0700

    free_dlistint prototype added

--- monty.h
+++ monty.h
@@ -52,4 +52,6 @@ void _push(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
 int verify_digit(char *num);
+void free_dlistint(stack_t *head);
+
 #endif
