84df93ce607ef04d39f8b894a8ada283a6be80fa
mattowsh <5089@holbertonschool.com>
2022-08-18 11:59:30 -0700

    prototypes were created: _swap / _add / _nop

--- monty.h
+++ monty.h
@@ -49,4 +49,8 @@ void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _push(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+
 #endif
