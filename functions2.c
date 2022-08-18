efecff27549af2dbcb29203a3f1124159c6e257a
mattowsh <5089@holbertonschool.com>
2022-08-18 13:02:45 -0700

    add function was creat4ed

---
+++ functions2.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/*
+ * _swap - Swaps the top two elements of the stack 
+ * @stack : double linked list
+ * @line_number : line number of the .m file
+ */
+void _swap(stack_t **stack, unsigned int line_number)
+{
+}
+	
+/*
+ * _add - Adds the top two elements of the stack
+ * @stack : double linked list
+ * @line_number : line number of the .m file
+ */
+void _add(stack_t **stack, unsigned int line_number)
+{
+	int sum = 0, counter = 0;
+	stack_t *h = *stack, *sig == NULL;
+
+	if (!h)
+		exit(EXIT_FAILURE);
+
+	/* to count number of nodes */
+	while (!h->next)
+	{
+		counter++;
+		h = h->next;
+	}
+		
+	if (counter < 2)
+	{
+		dprintf(STDERR_FILENO, "L<line_number>: can't add, stack too short", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	/* sum the two elements */
+	sum += h->n;
+	h = h->next;
+	sum += h->n;
+	h->n = sum;
+
+	/* deletes the first node */
+	h = h->prev;
+	*stack = h->next;
+	sig = h->next;
+	free(h);
+	if (sig)
+		sig->prev = NULL;
+
+	printf("%i\n", sum);
+}
+/*
+ * _nop - Doesn’t do anything
+ * @stack : double linked list
+ * @line_number : line number of the .m file
+ */
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack, (void) line_number;
+}
