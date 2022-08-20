79bf9f77bdf010ea5811296f43f14dea8e18a1d7
mattowsh <5089@holbertonschool.com>
2022-08-20 15:14:19 -0700

    openfile.c changed to the previous functional version

--- openfile.c
+++ openfile.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 
-/*
+/**
  * openfile - Opens the file passed like argument in ./monty execution
  * @filename : filename to be opened = av[1]
  *
@@ -10,38 +10,23 @@
 char *openfile(char *filename)
 {
 	ssize_t fd;
-	char *buffer, maxread[1024];
-	int recorre_buffer = 1024, length = 0;
+	char *buffer = malloc(40000);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO,"Error: Can't open file %s\n", filename);
-		exit(EXIT_FAILURE);
-	}
-	
-	while (recorre_buffer == 1024)
-	{
-		recorre_buffer = read(fd, maxread, 1024);
-		if (recorre_buffer == -1)
-		{
-			dprintf(STDERR_FILENO,"Error: Can't open file %s\n", filename);
-			exit(EXIT_FAILURE);
-		}
-		length += recorre_buffer;
-	}
-
-	buffer = malloc(sizeof(char) * length + 1);
 	if (!buffer)
 	{
-		write(STDERR_FILENO,"Error: malloc failed\n", 21);
+		write(STDERR_FILENO, "Error: malloc failed\n", 21);
 		exit(EXIT_FAILURE);
 	}
-	close(fd);
-
 	fd = open(filename, O_RDONLY);
-	read(fd, buffer, length);
-	close(fd);
 
+	if (fd == -1)
+	{
+		free(buffer);
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+	read(fd, buffer, 40000);
+
+	close(fd);
 	return (buffer);
 }
